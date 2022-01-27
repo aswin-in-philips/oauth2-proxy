cf97ee1e4ae805586b7e6d2353852174a7f80fda
aswin-in-philips <aswin.s@philips.com>
2022-01-27 10:51:18 +0530

    change

--- build/ps/Invoke-BlackDuckDetect.ps1
+++ build/ps/Invoke-BlackDuckDetect.ps1
@@ -6,7 +6,6 @@ param(
     $ApiToken = "OWFkOWM0NGMtM2FlMy00ODFiLThjMTctM2I1OTdkMTY2MTQ2OmNlMGI4NmNhLWRjMzAtNGU0Yy04NTIwLWEzZDI5NDFlNjdkMg==",
     $ProxyHost = "apac.zscaler.philips.com",
     $ProxyPort = "10015",
-    $DockerImageName = "quay.io/oauth2-proxy/oauth2-proxy:latest"
     $ProxyIgnoreHosts = "blackduck.philips.com"
 )
 
@@ -18,4 +17,4 @@ $detectScriptLocal = "$PSScriptRoot\detect.ps1"
 Invoke-WebRequest -Uri $detectScriptUrl -Method Get -OutFile $detectScriptLocal
 
 Import-Module $detectScriptLocal 
-Detect --detect.project.name=$ProjectName --detect.project.version.name=$ProjectVersionName --detect.source.path=$SourcePath --detect.docker.image=$DockerImageName --blackduck.url=$BlackduckUrl --blackduck.trust.cert=true --blackduck.api.token=$ApiToken --blackduck.proxy.host=$ProxyHost --blackduck.proxy.port=$ProxyPort --blackduck.proxy.ignored.hosts=$ProxyIgnoreHosts --detect.blackduck.signature.scanner.individual.file.matching=ALL --detect.detector.search.depth=6
+Detect --detect.project.name=$ProjectName --detect.project.version.name=$ProjectVersionName --detect.source.path=$SourcePath --detect.docker.image="quay.io/oauth2-proxy/oauth2-proxy:latest" --blackduck.url=$BlackduckUrl --blackduck.trust.cert=true --blackduck.api.token=$ApiToken --blackduck.proxy.host=$ProxyHost --blackduck.proxy.port=$ProxyPort --blackduck.proxy.ignored.hosts=$ProxyIgnoreHosts --detect.blackduck.signature.scanner.individual.file.matching=ALL --detect.detector.search.depth=6
