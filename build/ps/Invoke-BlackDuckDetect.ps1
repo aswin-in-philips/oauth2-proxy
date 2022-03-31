94e430d7f4a0e95165bcc81b9e6da98f337c8d5b
aswin-in-philips <aswin.s@philips.com>
2022-03-31 12:53:20 +0530

    Adding docker scan in blackduck

--- build/ps/Invoke-BlackDuckDetect.ps1
+++ build/ps/Invoke-BlackDuckDetect.ps1
@@ -19,4 +19,4 @@ $detectScriptLocal = "$PSScriptRoot\detect.ps1"
 Invoke-WebRequest -Uri $detectScriptUrl -Method Get -OutFile $detectScriptLocal
 
 Import-Module $detectScriptLocal 
-Detect --detect.project.name=$ProjectName --detect.project.version.name=$ProjectVersionName --detect.source.path=$SourcePath --blackduck.url=$BlackduckUrl --blackduck.trust.cert=true --blackduck.api.token=$ApiToken --blackduck.proxy.host=$ProxyHost --blackduck.proxy.port=$ProxyPort --blackduck.proxy.ignored.hosts=$ProxyIgnoreHosts --detect.blackduck.signature.scanner.individual.file.matching=ALL --detect.detector.search.depth=6
+Detect --detect.tools=DOCKER --detect.docker.image=$DockerImageName --detect.project.name=$ProjectName --detect.project.version.name=$ProjectVersionName --detect.source.path=$SourcePath --blackduck.url=$BlackduckUrl --blackduck.trust.cert=true --blackduck.api.token=$ApiToken --blackduck.proxy.host=$ProxyHost --blackduck.proxy.port=$ProxyPort --blackduck.proxy.ignored.hosts=$ProxyIgnoreHosts --detect.blackduck.signature.scanner.individual.file.matching=ALL --detect.detector.search.depth=6
