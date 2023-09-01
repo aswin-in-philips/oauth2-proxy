2c7543f38f27d52d7840ea962a76b8fd2be3dd0a
Eduardo Vozniak <Eduardo.Vozniak@philips.com>
2023-09-01 08:09:44 -0300

    Merging 7.4.0

--- providers/oidc_test.go
+++ providers/oidc_test.go
@@ -2,15 +2,19 @@ package providers
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
-	"github.com/coreos/go-oidc"
+	"github.com/coreos/go-oidc/v3/oidc"
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/options"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
+	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/encryption"
+	internaloidc "github.com/oauth2-proxy/oauth2-proxy/v7/pkg/providers/oidc"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,8 +26,11 @@ type redeemTokenResponse struct {
 	IDToken      string `json:"id_token,omitempty"`
 }
 
-func newOIDCProvider(serverURL *url.URL) *OIDCProvider {
-
+func newOIDCProvider(serverURL *url.URL, skipNonce bool) *OIDCProvider {
+	verificationOptions := internaloidc.IDTokenVerificationOptions{
+		AudienceClaims: []string{"aud"},
+		ClientID:       "https://test.myapp.com",
+	}
 	providerData := &ProviderData{
 		ProviderName: "oidc",
 		ClientID:     oidcClientID,
@@ -51,14 +58,17 @@ func newOIDCProvider(serverURL *url.URL) *OIDCProvider {
 		Scope:       "openid profile offline_access",
 		EmailClaim:  "email",
 		GroupsClaim: "groups",
-		Verifier: oidc.NewVerifier(
+		UserClaim:   "sub",
+		Verifier: internaloidc.NewVerifier(oidc.NewVerifier(
 			oidcIssuer,
 			mockJWKS{},
 			&oidc.Config{ClientID: oidcClientID},
-		),
+		), verificationOptions),
 	}
 
-	p := &OIDCProvider{ProviderData: providerData}
+	p := NewOIDCProvider(providerData, options.OIDCOptions{
+		InsecureSkipNonce: skipNonce,
+	})
 
 	return p
 }
@@ -90,12 +100,33 @@ func newOIDCServer(redeemJSON []byte, profileJSON []byte, introspectJSON []byte)
 func newTestOIDCSetup(redeemJSON []byte, profileJSON []byte, introspectJSON []byte) (*httptest.Server, *OIDCProvider) {
 	server := newOIDCServer(redeemJSON, profileJSON, introspectJSON)
 	serverURL, _ := url.Parse(server.URL)
-	provider := newOIDCProvider(serverURL)
+	provider := newOIDCProvider(serverURL, false)
 	return server, provider
 }
 
-func TestOIDCProviderRedeem(t *testing.T) {
+func TestOIDCProviderGetLoginURL(t *testing.T) {
+	serverURL := &url.URL{
+		Scheme: "https",
+		Host:   "oauth2proxy.oidctest",
+	}
+	provider := newOIDCProvider(serverURL, true)
 
+	n, err := encryption.Nonce(32)
+	assert.NoError(t, err)
+	nonce := base64.RawURLEncoding.EncodeToString(n)
+
+	// SkipNonce defaults to true
+	skipNonce := provider.GetLoginURL("http://redirect/", "", nonce, url.Values{})
+	assert.NotContains(t, skipNonce, "nonce")
+
+	provider.SkipNonce = false
+	withNonce := provider.GetLoginURL("http://redirect/", "", nonce, url.Values{})
+	assert.Contains(t, withNonce, fmt.Sprintf("nonce=%s", nonce))
+	assert.NotContains(t, withNonce, "code_challenge")
+	assert.NotContains(t, withNonce, "code_challenge_method")
+}
+
+func TestOIDCProviderRedeem(t *testing.T) {
 	idToken, _ := newSignedTestIDToken(defaultIDToken)
 	body, _ := json.Marshal(redeemTokenResponse{
 		AccessToken:  accessToken,
@@ -107,7 +138,7 @@ func TestOIDCProviderRedeem(t *testing.T) {
 	server, provider := newTestOIDCSetup(body, []byte(`{}`), []byte(`{}`))
 	defer server.Close()
 
-	session, err := provider.Redeem(context.Background(), provider.RedeemURL.String(), "code1234")
+	session, err := provider.Redeem(context.Background(), provider.RedeemURL.String(), "code1234", "")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, defaultIDToken.Email, session.Email)
 	assert.Equal(t, accessToken, session.AccessToken)
@@ -117,7 +148,6 @@ func TestOIDCProviderRedeem(t *testing.T) {
 }
 
 func TestOIDCProviderRedeem_custom_userid(t *testing.T) {
-
 	idToken, _ := newSignedTestIDToken(defaultIDToken)
 	body, _ := json.Marshal(redeemTokenResponse{
 		AccessToken:  accessToken,
@@ -131,424 +161,9 @@ func TestOIDCProviderRedeem_custom_userid(t *testing.T) {
 	provider.EmailClaim = "phone_number"
 	defer server.Close()
 
-	session, err := provider.Redeem(context.Background(), provider.RedeemURL.String(), "code1234")
+	session, err := provider.Redeem(context.Background(), provider.RedeemURL.String(), "code1234", "")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, defaultIDToken.Phone, session.Email)
-}
-
-func TestOIDCProvider_EnrichSession(t *testing.T) {
-	testCases := map[string]struct {
-		ExistingSession *sessions.SessionState
-		EmailClaim      string
-		GroupsClaim     string
-		ProfileJSON     map[string]interface{}
-		IntrospectJSON  map[string]interface{}
-		ExpectedError   error
-		ExpectedSession *sessions.SessionState
-	}{
-		"Already Populated": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				Groups:       []string{"already", "populated"},
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email":  "new@thing.com",
-				"groups": []string{"new", "thing"},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				Groups:           []string{"already", "populated"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Email": {
-			ExistingSession: &sessions.SessionState{
-				User:         "missing.email",
-				Groups:       []string{"already", "populated"},
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email":  "found@email.com",
-				"groups": []string{"new", "thing"},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "missing.email",
-				Email:            "found@email.com",
-				Groups:           []string{"already", "populated"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-
-		"Missing Email Only in Profile URL": {
-			ExistingSession: &sessions.SessionState{
-				User:         "missing.email",
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email": "found@email.com",
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "missing.email",
-				Email:            "found@email.com",
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Email with Custom Claim": {
-			ExistingSession: &sessions.SessionState{
-				User:         "missing.email",
-				Groups:       []string{"already", "populated"},
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "weird",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"weird":  "weird@claim.com",
-				"groups": []string{"new", "thing"},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "missing.email",
-				Email:            "weird@claim.com",
-				Groups:           []string{"already", "populated"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Email not in Profile URL": {
-			ExistingSession: &sessions.SessionState{
-				User:         "missing.email",
-				Groups:       []string{"already", "populated"},
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"groups": []string{"new", "thing"},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: errors.New("neither the id_token nor the profileURL set an email"),
-			ExpectedSession: &sessions.SessionState{
-				User:             "missing.email",
-				Groups:           []string{"already", "populated"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Groups": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				Groups:       nil,
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email":  "new@thing.com",
-				"groups": []string{"new", "thing"},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				Groups:           []string{"new", "thing"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Groups with Complex Groups in Profile URL": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				Groups:       nil,
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email": "new@thing.com",
-				"groups": []map[string]interface{}{
-					{
-						"groupId": "Admin Group Id",
-						"roles":   []string{"Admin"},
-					},
-				},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				Groups:           []string{"{\"groupId\":\"Admin Group Id\",\"roles\":[\"Admin\"]}"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Groups with Singleton Complex Group in Profile URL": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				Groups:       nil,
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email": "new@thing.com",
-				"groups": map[string]interface{}{
-					"groupId": "Admin Group Id",
-					"roles":   []string{"Admin"},
-				},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				Groups:           []string{"{\"groupId\":\"Admin Group Id\",\"roles\":[\"Admin\"]}"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Empty Groups Claims": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				Groups:       []string{},
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email":  "new@thing.com",
-				"groups": []string{"new", "thing"},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				Groups:           []string{},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Groups with Custom Claim": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				Groups:       nil,
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "roles",
-			ProfileJSON: map[string]interface{}{
-				"email": "new@thing.com",
-				"roles": []string{"new", "thing", "roles"},
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				Groups:           []string{"new", "thing", "roles"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Groups String Profile URL Response": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				Groups:       nil,
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email":  "new@thing.com",
-				"groups": "singleton",
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				Groups:           []string{"singleton"},
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Missing Groups in both Claims and Profile URL": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email": "new@thing.com",
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active": true,
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWV9",
-			},
-		},
-		"Introspection Response added in session": {
-			ExistingSession: &sessions.SessionState{
-				User:         "already",
-				Email:        "already@populated.com",
-				IDToken:      idToken,
-				AccessToken:  accessToken,
-				RefreshToken: refreshToken,
-			},
-			EmailClaim:  "email",
-			GroupsClaim: "groups",
-			ProfileJSON: map[string]interface{}{
-				"email": "new@thing.com",
-			},
-			IntrospectJSON: map[string]interface{}{
-				"active":     true,
-				"exp":        1613553280,
-				"iat":        1613549680,
-				"sub":        "00u65ah10oSUDoNd65d6",
-				"aud":        "0oa5t6ts1XZgPOOSA5d6",
-				"iss":        "https://dev.authprovider.com/oauth2/default",
-				"jti":        "ID.ShxppK0vysORHsr8S9GSPPRV-Dy1PFhqx3fPYwtijc0",
-				"token_type": "Bearer",
-				"at_hash":    "_1EsbE1ZpyMOLiu-VdBTlg",
-				"idp":        "0oa5t8x90SALHBBk85d6",
-				"auth_time":  1613549026,
-				"amr":        []string{"pwd"},
-			},
-			ExpectedError: nil,
-			ExpectedSession: &sessions.SessionState{
-				User:             "already",
-				Email:            "already@populated.com",
-				IDToken:          idToken,
-				AccessToken:      accessToken,
-				RefreshToken:     refreshToken,
-				IntrospectClaims: "eyJhY3RpdmUiOnRydWUsImFtciI6WyJwd2QiXSwiYXRfaGFzaCI6Il8xRXNiRTFacHlNT0xpdS1WZEJUbGciLCJhdWQiOiIwb2E1dDZ0czFYWmdQT09TQTVkNiIsImF1dGhfdGltZSI6MTYxMzU0OTAyNiwiZXhwIjoxNjEzNTUzMjgwLCJpYXQiOjE2MTM1NDk2ODAsImlkcCI6IjBvYTV0OHg5MFNBTEhCQms4NWQ2IiwiaXNzIjoiaHR0cHM6Ly9kZXYuYXV0aHByb3ZpZGVyLmNvbS9vYXV0aDIvZGVmYXVsdCIsImp0aSI6IklELlNoeHBwSzB2eXNPUkhzcjhTOUdTUFBSVi1EeTFQRmhxeDNmUFl3dGlqYzAiLCJzdWIiOiIwMHU2NWFoMTBvU1VEb05kNjVkNiIsInRva2VuX3R5cGUiOiJCZWFyZXIifQ==",
-			},
-		},
-	}
-	for testName, tc := range testCases {
-		t.Run(testName, func(t *testing.T) {
-			profileJSON, err := json.Marshal(tc.ProfileJSON)
-			assert.NoError(t, err)
-
-			introspectJSON, err := json.Marshal(tc.IntrospectJSON)
-			assert.NoError(t, err)
-
-			server, provider := newTestOIDCSetup([]byte(`{}`), profileJSON, introspectJSON)
-			assert.NoError(t, err)
-
-			provider.EmailClaim = tc.EmailClaim
-			provider.GroupsClaim = tc.GroupsClaim
-			defer server.Close()
-
-			err = provider.EnrichSession(context.Background(), tc.ExistingSession)
-			assert.Equal(t, tc.ExpectedError, err)
-			assert.Equal(t, *tc.ExpectedSession, *tc.ExistingSession)
-		})
-	}
 }
 
 func TestOIDCProviderRefreshSessionIfNeededWithoutIdToken(t *testing.T) {
@@ -574,7 +189,7 @@ func TestOIDCProviderRefreshSessionIfNeededWithoutIdToken(t *testing.T) {
 		User:         "11223344",
 	}
 
-	refreshed, err := provider.RefreshSessionIfNeeded(context.Background(), existingSession)
+	refreshed, err := provider.RefreshSession(context.Background(), existingSession)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, refreshed, true)
 	assert.Equal(t, "janedoe@example.com", existingSession.Email)
@@ -607,7 +222,7 @@ func TestOIDCProviderRefreshSessionIfNeededWithIdToken(t *testing.T) {
 		Email:        "changeit",
 		User:         "changeit",
 	}
-	refreshed, err := provider.RefreshSessionIfNeeded(context.Background(), existingSession)
+	refreshed, err := provider.RefreshSession(context.Background(), existingSession)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, refreshed, true)
 	assert.Equal(t, defaultIDToken.Email, existingSession.Email)
@@ -647,11 +262,15 @@ func TestOIDCProviderCreateSessionFromToken(t *testing.T) {
 			ExpectedGroups: []string{"test:c", "test:d"},
 		},
 		"Complex Groups Claim": {
-			IDToken:        complexGroupsIDToken,
-			GroupsClaim:    "groups",
-			ExpectedUser:   "123456789",
-			ExpectedEmail:  "complex@claims.com",
-			ExpectedGroups: []string{"{\"groupId\":\"Admin Group Id\",\"roles\":[\"Admin\"]}"},
+			IDToken:       complexGroupsIDToken,
+			GroupsClaim:   "groups",
+			ExpectedUser:  "123456789",
+			ExpectedEmail: "complex@claims.com",
+			ExpectedGroups: []string{
+				"{\"groupId\":\"Admin Group Id\",\"roles\":[\"Admin\"]}",
+				"12345",
+				"Just::A::String",
+			},
 		},
 	}
 	for testName, tc := range testCases {
